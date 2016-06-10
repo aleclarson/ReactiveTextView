e1b974ba3091a88f37b2a8eb7b4e2dc795183d0b
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-06-09 17:43:36 -0700

    1.0.0

---
+++ js/src/ReactiveTextView.js
@@ -0,0 +1,60 @@
+var Component, Maybe, NativeValue, ReactiveGetter, Style, TextView, assert, objectify, ref, type;
+
+ref = require("component"), Style = ref.Style, TextView = ref.TextView, Component = ref.Component, NativeValue = ref.NativeValue;
+
+ReactiveGetter = require("ReactiveGetter");
+
+objectify = require("objectify");
+
+assert = require("assert");
+
+Maybe = require("Maybe");
+
+type = Component("ReactiveTextView");
+
+type.propTypes = {
+  text: NativeValue.Maybe,
+  getText: Maybe(Function.Kind),
+  style: Style
+};
+
+type.initProps(function(props) {
+  return assert((props.text != null) || (props.getText != null), function() {
+    return {
+      reason: "Either 'text' or 'getText' must be defined in 'props'!"
+    };
+  });
+});
+
+type.defineValues({
+  textProps: function() {
+    return objectify({
+      keys: TextView.propTypes,
+      values: this.props
+    });
+  }
+});
+
+type.defineNativeValues({
+  text: function() {
+    return this.props.text || ReactiveGetter(this.props.getText);
+  }
+});
+
+type.defineListeners(function() {
+  return this.text.didSet((function(_this) {
+    return function() {
+      try {
+        return _this.forceUpdate();
+      } catch (error) {}
+    };
+  })(this));
+});
+
+type.render(function() {
+  return TextView(this.textProps, this.text.value || "");
+});
+
+module.exports = type.build();
+
+//# sourceMappingURL=../../map/src/ReactiveTextView.map
---
+++ src/ReactiveTextView.coffee
@@ -0,0 +1,43 @@
+
+{ Style
+  TextView
+  Component
+  NativeValue } = require "component"
+
+ReactiveGetter = require "ReactiveGetter"
+objectify = require "objectify"
+assert = require "assert"
+Maybe = require "Maybe"
+
+type = Component "ReactiveTextView"
+
+type.propTypes =
+  text: NativeValue.Maybe
+  getText: Maybe(Function.Kind)
+  style: Style
+
+type.initProps (props) ->
+  assert props.text? or props.getText?, ->
+    reason: "Either 'text' or 'getText' must be defined in 'props'!"
+
+type.defineValues
+
+  textProps: -> objectify
+    keys: TextView.propTypes
+    values: @props
+
+type.defineNativeValues
+
+  text: ->
+    @props.text or
+    ReactiveGetter @props.getText
+
+type.defineListeners ->
+
+  @text.didSet =>
+    try @forceUpdate()
+
+type.render ->
+  TextView @textProps, @text.value or ""
+
+module.exports = type.build()
